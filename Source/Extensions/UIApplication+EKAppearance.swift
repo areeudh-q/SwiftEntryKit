4df83312634c45ff2e4b6dddfe7e1ddbc8a2a592
didbbs <38516031+areeudh-q@users.noreply.github.com>
2025-06-18 13:55:35 +0800

    Update UIApplication+EKAppearance.swift

--- Source/Extensions/UIApplication+EKAppearance.swift
+++ Source/Extensions/UIApplication+EKAppearance.swift
@@ -15,4 +15,53 @@ extension UIApplication {
         UIApplication.shared.isStatusBarHidden = !appearance.visible
         UIApplication.shared.statusBarStyle = appearance.style
     }
+
+    var c_window: UIWindow? {
+        if #available(iOS 13.0, *) {
+            if let window = UIApplication.shared.connectedScenes.filter({ $0.activationState == .foregroundActive }).map({ $0 as? UIWindowScene }).compactMap({ $0 }).first?.windows.filter({ $0.isKeyWindow }).first {
+                return window
+            } else if let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first {
+                return window
+            } else if let window = UIApplication.shared.delegate?.window {
+                return window
+            } else {
+                return nil
+            }
+        } else {
+            if let window = UIApplication.shared.delegate?.window {
+                return window
+            } else {
+                return nil
+            }
+        }
+    }
+
+    var c_rootViewController: UIViewController? {
+        if let vc = c_window?.rootViewController {
+            return vc
+        }
+        return c_window?.rootViewController?.c_topViewController
+    }
+}
+
+
+extension UIViewController {
+    var c_topViewController: UIViewController {
+        if presentedViewController == nil {
+            return self
+        }
+
+        if let navigation = presentedViewController as? UINavigationController {
+            return navigation.visibleViewController!.c_topViewController
+        }
+
+        if let tab = presentedViewController as? UITabBarController {
+            if let selectedTab = tab.selectedViewController {
+                return selectedTab.c_topViewController
+            }
+            return tab.c_topViewController
+        }
+
+        return presentedViewController!.c_topViewController
+    }
 }
